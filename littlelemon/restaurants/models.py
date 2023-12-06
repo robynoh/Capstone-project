ba95831e19298f18c575084096952f78f2ac62ad
robinsonagaga <robinsonagaga@gmail.com>
2023-12-06 02:20:18 -0800

    my third commit

--- littlelemon/restaurants/models.py
+++ littlelemon/restaurants/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    Name= models.CharField(max_length=255)
+    no_of_guests=models.IntegerField()
+    BookingDate=models.DateField()
+    
+class Menu(models.Model):
+    Title= models.CharField(max_length=255)
+    price=models.DecimalField(max_digits = 10,decimal_places = 2) 
+    Inventory=models.IntegerField()
